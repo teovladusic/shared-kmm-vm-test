e3885fb3531154ae282896f770d22f1ba619de7a
Teo Vladušić <teo.vladusic@gmail.com>
2023-10-10 13:42:40 +0200

    doktor

--- shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/SharedViewModel.kt
+++ shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/SharedViewModel.kt
@@ -5,9 +5,12 @@ package com.puzzle_agency.sharedvmtest
 import com.rickclephas.kmm.viewmodel.KMMViewModel
 import com.rickclephas.kmm.viewmodel.MutableStateFlow
 import com.rickclephas.kmm.viewmodel.coroutineScope
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import kotlinx.coroutines.flow.asStateFlow
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -18,12 +21,22 @@ class SharedViewModel: KMMViewModel() {
     @NativeCoroutinesState
     val state = _state.asStateFlow()
 
+    private val channel = Channel<Boolean>()
+
+    @NativeCoroutines
+    val oneTimeEventFlow = channel.receiveAsFlow()
+
     init {
         viewModelScope.coroutineScope.launch {
             while (true) {
                 delay(2000)
                 _state.update { it.copy(isLoading = !it.isLoading) }
             }
+        }
+
+        viewModelScope.coroutineScope.launch {
+            delay(5_000)
+            channel.send(true)
         }
     }
 }
