44b543e2ac408d05756b478be7659c240983b6b4
Teo Vladušić <teo.vladusic@gmail.com>
2023-10-19 17:52:58 +0200

    a

--- shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/ViewModelHelper.kt
+++ shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/ViewModelHelper.kt
@@ -1,8 +1,13 @@
 package com.puzzle_agency.sharedvmtest
 
+import com.puzzle_agency.navigation.IAppNavigator
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class ViewModelHelper : KoinComponent {
     val sharedViewModel: SharedViewModel by inject()
+}
+
+class Helper: KoinComponent {
+    val appNavigator: IAppNavigator by inject()
 }
