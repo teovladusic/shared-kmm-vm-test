15961f42b2bcd7d7f830e9f4c149fe7370c6dce8
Teo Vladušić <teo.vladusic@gmail.com>
2023-10-21 14:04:27 +0200

    fix

--- androidApp/src/main/java/com/puzzle_agency/sharedvmtest/android/navigation/DestinationRoute.kt
+++ androidApp/src/main/java/com/puzzle_agency/sharedvmtest/android/navigation/DestinationRoute.kt
@@ -3,12 +3,14 @@ package com.puzzle_agency.sharedvmtest.android.navigation
 import com.puzzle_agency.sharedvmtest.android.destinations.Authentication1Destination
 import com.puzzle_agency.sharedvmtest.android.destinations.Authentication2Destination
 import com.puzzle_agency.sharedvmtest.android.destinations.HomeDestination
+import com.puzzle_agency.sharedvmtest.navigation.destination.Authentication
 import com.puzzle_agency.sharedvmtest.navigation.destination.DestinationScreen
+import com.puzzle_agency.sharedvmtest.navigation.destination.Main
 
 
 val DestinationScreen.route: String
     get() = when (this) {
-        DestinationScreen.Auth1 -> Authentication1Destination.route
-        is DestinationScreen.Auth2 -> Authentication2Destination(testObject).route
-        DestinationScreen.Home -> HomeDestination.route
+        Authentication.Auth1 -> Authentication1Destination.route
+        is Authentication.Auth2 -> Authentication2Destination(testObject).route
+        Main.Home -> HomeDestination.route
     }
--- shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/navigation/destination/DestinationScreen.kt
+++ shared/src/commonMain/kotlin/com/puzzle_agency/sharedvmtest/navigation/destination/DestinationScreen.kt
@@ -3,13 +3,4 @@ package com.puzzle_agency.sharedvmtest.navigation.destination
 import com.puzzle_agency.sharedvmtest.Parcelable
 import com.puzzle_agency.sharedvmtest.Parcelize
 
-sealed class DestinationScreen {
-
-    data object Home: DestinationScreen()
-
-    data object Auth1: DestinationScreen()
-    data class Auth2(val testObject: TestObject): DestinationScreen()
-}
-
-@Parcelize
-data class TestObject(val title: String): Parcelable
+sealed class DestinationScreen
